e3ac84afa83eb2447eb58c7964d971bf609c6679
Danilo Zutin <dgzutin@gmail.com>
2015-03-30 15:13:22 +0200

    create jobRecord entity

---
+++ src/AppBundle/Entity/JobRecord.php
@@ -0,0 +1,568 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: garbi
+ * Date: 3/29/15
+ * Time: 11:12 PM
+ */
+// src/AppBundle/Entity/ExperimentEngine.php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="JobRecord")
+ */
+class JobRecord
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $expId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $labServerId; //New field: specifies the ID of the "virtual" lab server
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $providerId;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $rlmsAssignedId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $priority;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $jobStatus; // (1)QUEUED , (2)IN PROGRESS, (3)COMPLETE, (4)CANCELLED
+
+    /**
+     * @ORM\Column(type="datetimetz")
+     */
+    protected $submitTime; //Time when the job was submitted
+
+    /**
+     * @ORM\Column(type="datetimetz")
+     */
+    protected $executionTime; //Time when execution starts, or when job is dequeued
+    /**
+     * @ORM\Column(type="datetimetz")
+     */
+    protected $endTime; //Time when execution finishes or is cancelled
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $execElapsed; //Time (sec) that the experiment needed to run
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $jobElapsed;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $estExecTime; //Estimated time (sec) the experiment will need to run
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $queueAtInsert; //Job position in the queue at insert time
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $processingEngine; //Id of the engine (experiment engine) executing the experiment
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $expSpecification;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $expResults;
+
+    /**
+     * @ORM\Column(type="string", length=2000)
+     */
+    protected $errorReport;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $errorOccurred;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $downloaded;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $opaque; //Optional field, used to transfer additional information if necessary.
+
+
+
+    /**
+     * Get expId
+     *
+     * @return integer 
+     */
+    public function getExpId()
+    {
+        return $this->expId;
+    }
+
+    /**
+     * Set labServerId
+     *
+     * @param integer $labServerId
+     * @return JobRecord
+     */
+    public function setLabServerId($labServerId)
+    {
+        $this->labServerId = $labServerId;
+
+        return $this;
+    }
+
+    /**
+     * Get labServerId
+     *
+     * @return integer 
+     */
+    public function getLabServerId()
+    {
+        return $this->labServerId;
+    }
+
+    /**
+     * Set providerId
+     *
+     * @param string $providerId
+     * @return JobRecord
+     */
+    public function setProviderId($providerId)
+    {
+        $this->providerId = $providerId;
+
+        return $this;
+    }
+
+    /**
+     * Get providerId
+     *
+     * @return string 
+     */
+    public function getProviderId()
+    {
+        return $this->providerId;
+    }
+
+    /**
+     * Set rlmsAssignedId
+     *
+     * @param string $rlmsAssignedId
+     * @return JobRecord
+     */
+    public function setRlmsAssignedId($rlmsAssignedId)
+    {
+        $this->rlmsAssignedId = $rlmsAssignedId;
+
+        return $this;
+    }
+
+    /**
+     * Get rlmsAssignedId
+     *
+     * @return string 
+     */
+    public function getRlmsAssignedId()
+    {
+        return $this->rlmsAssignedId;
+    }
+
+    /**
+     * Set priority
+     *
+     * @param integer $priority
+     * @return JobRecord
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * Get priority
+     *
+     * @return integer 
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * Set jobStatus
+     *
+     * @param integer $jobStatus
+     * @return JobRecord
+     */
+    public function setJobStatus($jobStatus)
+    {
+        $this->jobStatus = $jobStatus;
+
+        return $this;
+    }
+
+    /**
+     * Get jobStatus
+     *
+     * @return integer 
+     */
+    public function getJobStatus()
+    {
+        return $this->jobStatus;
+    }
+
+    /**
+     * Set submitTime
+     *
+     * @param \DateTime $submitTime
+     * @return JobRecord
+     */
+    public function setSubmitTime($submitTime)
+    {
+        $this->submitTime = $submitTime;
+
+        return $this;
+    }
+
+    /**
+     * Get submitTime
+     *
+     * @return \DateTime 
+     */
+    public function getSubmitTime()
+    {
+        return $this->submitTime;
+    }
+
+    /**
+     * Set executionTime
+     *
+     * @param \DateTime $executionTime
+     * @return JobRecord
+     */
+    public function setExecutionTime($executionTime)
+    {
+        $this->executionTime = $executionTime;
+
+        return $this;
+    }
+
+    /**
+     * Get executionTime
+     *
+     * @return \DateTime 
+     */
+    public function getExecutionTime()
+    {
+        return $this->executionTime;
+    }
+
+    /**
+     * Set endTime
+     *
+     * @param \DateTime $endTime
+     * @return JobRecord
+     */
+    public function setEndTime($endTime)
+    {
+        $this->endTime = $endTime;
+
+        return $this;
+    }
+
+    /**
+     * Get endTime
+     *
+     * @return \DateTime 
+     */
+    public function getEndTime()
+    {
+        return $this->endTime;
+    }
+
+    /**
+     * Set execElapsed
+     *
+     * @param integer $execElapsed
+     * @return JobRecord
+     */
+    public function setExecElapsed($execElapsed)
+    {
+        $this->execElapsed = $execElapsed;
+
+        return $this;
+    }
+
+    /**
+     * Get execElapsed
+     *
+     * @return integer 
+     */
+    public function getExecElapsed()
+    {
+        return $this->execElapsed;
+    }
+
+    /**
+     * Set jobElapsed
+     *
+     * @param integer $jobElapsed
+     * @return JobRecord
+     */
+    public function setJobElapsed($jobElapsed)
+    {
+        $this->jobElapsed = $jobElapsed;
+
+        return $this;
+    }
+
+    /**
+     * Get jobElapsed
+     *
+     * @return integer 
+     */
+    public function getJobElapsed()
+    {
+        return $this->jobElapsed;
+    }
+
+    /**
+     * Set estExecTime
+     *
+     * @param integer $estExecTime
+     * @return JobRecord
+     */
+    public function setEstExecTime($estExecTime)
+    {
+        $this->estExecTime = $estExecTime;
+
+        return $this;
+    }
+
+    /**
+     * Get estExecTime
+     *
+     * @return integer 
+     */
+    public function getEstExecTime()
+    {
+        return $this->estExecTime;
+    }
+
+    /**
+     * Set queueAtInsert
+     *
+     * @param integer $queueAtInsert
+     * @return JobRecord
+     */
+    public function setQueueAtInsert($queueAtInsert)
+    {
+        $this->queueAtInsert = $queueAtInsert;
+
+        return $this;
+    }
+
+    /**
+     * Get queueAtInsert
+     *
+     * @return integer 
+     */
+    public function getQueueAtInsert()
+    {
+        return $this->queueAtInsert;
+    }
+
+    /**
+     * Set processingEngine
+     *
+     * @param integer $processingEngine
+     * @return JobRecord
+     */
+    public function setProcessingEngine($processingEngine)
+    {
+        $this->processingEngine = $processingEngine;
+
+        return $this;
+    }
+
+    /**
+     * Get processingEngine
+     *
+     * @return integer 
+     */
+    public function getProcessingEngine()
+    {
+        return $this->processingEngine;
+    }
+
+    /**
+     * Set expSpecification
+     *
+     * @param string $expSpecification
+     * @return JobRecord
+     */
+    public function setExpSpecification($expSpecification)
+    {
+        $this->expSpecification = $expSpecification;
+
+        return $this;
+    }
+
+    /**
+     * Get expSpecification
+     *
+     * @return string 
+     */
+    public function getExpSpecification()
+    {
+        return $this->expSpecification;
+    }
+
+    /**
+     * Set expResults
+     *
+     * @param string $expResults
+     * @return JobRecord
+     */
+    public function setExpResults($expResults)
+    {
+        $this->expResults = $expResults;
+
+        return $this;
+    }
+
+    /**
+     * Get expResults
+     *
+     * @return string 
+     */
+    public function getExpResults()
+    {
+        return $this->expResults;
+    }
+
+    /**
+     * Set errorReport
+     *
+     * @param string $errorReport
+     * @return JobRecord
+     */
+    public function setErrorReport($errorReport)
+    {
+        $this->errorReport = $errorReport;
+
+        return $this;
+    }
+
+    /**
+     * Get errorReport
+     *
+     * @return string 
+     */
+    public function getErrorReport()
+    {
+        return $this->errorReport;
+    }
+
+    /**
+     * Set errorOccurred
+     *
+     * @param boolean $errorOccurred
+     * @return JobRecord
+     */
+    public function setErrorOccurred($errorOccurred)
+    {
+        $this->errorOccurred = $errorOccurred;
+
+        return $this;
+    }
+
+    /**
+     * Get errorOccurred
+     *
+     * @return boolean 
+     */
+    public function getErrorOccurred()
+    {
+        return $this->errorOccurred;
+    }
+
+    /**
+     * Set downloaded
+     *
+     * @param boolean $downloaded
+     * @return JobRecord
+     */
+    public function setDownloaded($downloaded)
+    {
+        $this->downloaded = $downloaded;
+
+        return $this;
+    }
+
+    /**
+     * Get downloaded
+     *
+     * @return boolean 
+     */
+    public function getDownloaded()
+    {
+        return $this->downloaded;
+    }
+
+    /**
+     * Set opaque
+     *
+     * @param string $opaque
+     * @return JobRecord
+     */
+    public function setOpaque($opaque)
+    {
+        $this->opaque = $opaque;
+
+        return $this;
+    }
+
+    /**
+     * Get opaque
+     *
+     * @return string 
+     */
+    public function getOpaque()
+    {
+        return $this->opaque;
+    }
+}
