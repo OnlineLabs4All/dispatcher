67cf30b942fb43a4ed4a21d9c79aaf59aeb19977
Danilo Zutin <dgzutin@gmail.com>
2015-04-10 19:30:10 +0200

    create labInfo model response

---
+++ src/AppBundle/Model/Subscriber/LabInfo.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: garbi
+ * Date: 4/10/15
+ * Time: 12:36 PM
+ */
+
+namespace AppBundle\Model\Subscriber;
+
+use SimpleXMLElement;
+
+class LabInfo{
+
+    public $name;
+
+    public $description;
+
+    public $owner_institution;
+
+    public $status;
+
+    public function serialize($format)
+    {
+        if ($format == 'xml'){
+
+            $labInfo_array = array_flip((array)$this);
+
+            $xml_labInfo = new SimpleXMLElement('<labInfo/>');
+            array_walk_recursive($labInfo_array, array ($xml_labInfo, 'addChild'));
+
+            return $xml_labInfo->asXML();
+        }
+
+        $json_labInfo = json_encode($this);
+        return $json_labInfo;
+
+    }
+}
