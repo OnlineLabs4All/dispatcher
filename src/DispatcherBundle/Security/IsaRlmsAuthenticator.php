a3662fa4091ad954124ce68507f3ea81db10d2f2
Danilo Zutin <dgzutin@gmail.com>
2015-10-15 18:30:55 +1000

    add authenticate method for UQ iLab lab server API

--- src/DispatcherBundle/Security/IsaRlmsAuthenticator.php
+++ src/DispatcherBundle/Security/IsaRlmsAuthenticator.php
@@ -107,5 +107,46 @@ class IsaRlmsAuthenticator{
         return array('authenticated' => false, 'fault' => 'Provided Service Broker GUID is not registered');
     }
 
+    public function authenticateMethodUqBroker($jsonRequest, $token, $sbGuid, $labserverId)
+    {
+        //remove token to apply hmac algorithm
+        $jsonRequest->token = '';
+        $data = json_encode($jsonRequest);
+
+        $broker = $this
+            ->em
+            ->getRepository('DispatcherBundle:Rlms')
+            ->findOneBy(array('Guid' => $sbGuid));
+
+        if ($broker != null) //if Broker exists, authenticate labServer
+        {
+            $labServer = $this
+                ->em
+                ->getRepository('DispatcherBundle:LabServer')
+                ->findOneBy(array('id' => $labserverId));
+
+            if ($labServer != null)
+            {
+                if ($token == base64_encode(hash_hmac('sha1', $sbGuid.$data,$labServer->getPassKey(),true)))
+                {
+                    $mapping = $this
+                        ->em
+                        ->getRepository('DispatcherBundle:LsToRlmsMapping')
+                        ->findOneBy(array('labServerId' => $labServer->getId(), 'rlmsId' => $broker->getId()));
+
+                    if ($mapping != null)
+                    {
+                        return array('authenticated' => true, 'fault' => '');
+                    }
+                    return array('authenticated' => false, 'fault' => 'Token is correct, but Broker and Lab Server are not mapped to each other. Contact your Experiment Dispatcher administrator');
+
+                }
+                return array('authenticated' => false, 'fault' => 'Unauthorized, could not verify token');
+            }
+            return array('authenticated' => false, 'fault' => 'Provided Lab Server Id does not exit');
+        }
+        return array('authenticated' => false, 'fault' => 'Provided Service Broker GUID is not registered');
+    }
+
 }
 
