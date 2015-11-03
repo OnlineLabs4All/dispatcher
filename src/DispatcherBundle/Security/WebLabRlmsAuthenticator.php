fe06b363f4cba91212e05356c91cf22f029dc89b
Danilo Zutin <dgzutin@gmail.com>
2015-11-03 21:03:35 +0100

    verify session and return existing  session if valid

--- src/DispatcherBundle/Security/WebLabRlmsAuthenticator.php
+++ src/DispatcherBundle/Security/WebLabRlmsAuthenticator.php
@@ -26,6 +26,23 @@ class WebLabRlmsAuthenticator
 
         if ($rlms != null){
 
+            //check if lab session already exists and is valid
+            $labSession = $this
+                ->em
+                ->getRepository('DispatcherBundle:LabSession')
+                ->findOneBy(array('rlmsId' => $rlms->getId() ));
+
+            if ($labSession != null){
+
+                $now = date_create(date('Y-m-d\TH:i:sP'));
+                $end = date_create($labSession->getEndDate());
+                if ($now < $end){
+                    $response = array('is_exception' => false,
+                        'exception' => '',
+                        'session_id' => $labSession->getSessionId());
+                    return $response;
+                }
+            }
             $session_duration = '604800';
             $dateNow = date('Y-m-d\TH:i:sP');
             $startDate = date_create($dateNow);
@@ -43,7 +60,8 @@ class WebLabRlmsAuthenticator
         }
 
         $response = array('is_exception' => true,
-                          'exception' => 'Invalid credentials or you do not have permissions to access any lab',
+                          'message' => 'Invalid credentials',
+                          'code' => 'JSON:Client.InvalidCredentials',
                           'session_id'=> '');
         return $response;
     }
@@ -64,6 +82,33 @@ class WebLabRlmsAuthenticator
             return null;
         }
         return null;
+    }
+
+    public function setSessionLabServer($session_id, $exp_cat, $exp_name)
+    {
+        $labSession = $this
+            ->em
+            ->getRepository('DispatcherBundle:LabSession')
+            ->findOneBy(array('session_id' => $session_id ));
+
+        if ($labSession != null){
+
+            $lab = $this
+                ->em
+                ->getRepository('DispatcherBundle:LabServer')
+                ->findOneBy(array('exp_category' => $exp_cat, 'exp_name' => $exp_name));
+
+            if ($lab != null){
+                $labSession->setLabServerId($lab->getId());
+                $this->em->persist($labSession);
+                $this->em->flush();
+
+                return true;
+            }
+            return false;
+        }
+        return false;
+
     }
 
 }
