d26f15acc7c5e376c6c91bc1838cd0068cf52944
Danilo Zutin <dgzutin@gmail.com>
2015-11-05 15:28:28 +0100

    implement get_experiment_use_by_id and get_experiment_uses_by_id services

--- src/DispatcherBundle/Controller/WeblabDeustoApiController.php
+++ src/DispatcherBundle/Controller/WeblabDeustoApiController.php
@@ -66,9 +66,9 @@ class WeblabDeustoApiController extends Controller
      */
     public function weblabDeustoAction(Request $request)
     {
-        //$myfile = fopen('webservice.txt','w') or die("Unable to open file");
-        //fwrite($myfile, $request->getContent());
-        //fclose($myfile);
+        $myfile = fopen('webservice.txt','w') or die("Unable to open file");
+        fwrite($myfile, $request->getContent());
+        fclose($myfile);
 
         $requestJson = json_decode($request->getContent());
         $method = $requestJson->method;
@@ -114,33 +114,82 @@ class WeblabDeustoApiController extends Controller
             case 'reserve_experiment':
 
                 $session_id = $requestJson->params->session_id->id;
-                $labSession = $webLabAuthenticator->validateSessionById($session_id);
+                $validationResponse = $webLabAuthenticator->validateSessionById($session_id);
 
-                if ($labSession != null) {
-                    $rlmsId = $labSession->getRlmsId();
-                    $responseJson = $webLabService->reserveExperiment($params, $rlmsId);
+                if ($validationResponse['is_exception']) {
+                    $responseJson = $validationResponse;
                 }
                 else{
-                    $responseJson = array('is_exception' => true,
-                        'message' => 'Session does not exist or has already expired. Please login again.',
-                        'code' => 'Client.SessionNotFound');
+                    $labSession = $validationResponse['labSession'];
+                    $rlmsId = $labSession->getRlmsId();
+                    $responseJson = $webLabService->reserveExperiment($params, $rlmsId);
                 }
                 break;
             case 'get_reservation_status':
 
                 $session_id = $request->cookies->get('weblabsessionid');
-                $labSession = $webLabAuthenticator->validateSessionById($session_id);
+                $validationResponse = $webLabAuthenticator->validateSessionById($session_id);
 
-                if ($labSession != null) {
-                    $rlmsId = $labSession->getRlmsId();
-                    $labServerId = $labSession->getLabServerId();
-                    $responseJson = $webLabService->getReservationStatus($params, $rlmsId);
+                if ($validationResponse['is_exception']) {
+                    $responseJson = $validationResponse;
                 }
                 else{
-                    $responseJson = array('is_exception' => true,
-                        'message' => 'Session does not exist or has already expired. Please login again.',
-                        'code' => 'Client.SessionNotFound');
+                    $labSession = $validationResponse['labSession'];
+                    $rlmsId = $labSession->getRlmsId();
+                    $responseJson = $webLabService->getReservationStatus($params, $rlmsId);
                 }
+                break;
+            case 'finished_experiment':
+                $session_id = $request->cookies->get('weblabsessionid');
+                $validationResponse = $webLabAuthenticator->validateSessionById($session_id);
+
+                if ($validationResponse['is_exception']) {
+                    $responseJson = $validationResponse;
+                }
+                else{
+                    $responseJson = array('result' => array(),
+                        'is_exception' => false);
+                }
+
+                break;
+            case 'get_experiment_use_by_id':
+                $session_id = $requestJson->params->session_id->id;
+                $validationResponse = $webLabAuthenticator->validateSessionById($session_id);
+
+                if ($validationResponse['is_exception']) {
+                    $responseJson = $validationResponse;
+                }
+                else{ //session is valid, process request
+                    $reservation_id = $requestJson->params->reservation_id->id;
+                    $labSession = $validationResponse['labSession'];
+                    $rlmsId = $labSession->getRlmsId();
+                    $responseJson = array('is_exception' => false,
+                                           'result' => $webLabService->getExperimentUseById($reservation_id, $rlmsId));
+                }
+                break;
+            case 'get_experiment_uses_by_id':
+
+                $session_id = $requestJson->params->session_id->id;
+                $validationResponse = $webLabAuthenticator->validateSessionById($session_id);
+
+                if ($validationResponse['is_exception']) {
+                    $responseJson = $validationResponse;
+                }
+                else{ //session is valid, process request
+                    $reservation_ids = $requestJson->params->reservation_ids; //array of reservation IDs or experiment ID in iLab parlance
+                    $labSession = $validationResponse['labSession'];
+                    $rlmsId = $labSession->getRlmsId();
+                    $responseJson = array('is_exception' => false,
+                        'result' => $webLabService->getExperimentUsesById($reservation_ids, $rlmsId));
+                }
+
+                break;
+            default:
+                $responseJson = array('is_exception' => true,
+                    'message' => 'Could not parse request',
+                    'code' => 'Server.UserProcessing');
+                break;
+
         }
 
         $response->headers->set('Content-type', 'application/json');
--- src/DispatcherBundle/Services/WebLabDeustoServices.php
+++ src/DispatcherBundle/Services/WebLabDeustoServices.php
@@ -5,6 +5,7 @@
  * Time: 20:14
  */
 namespace DispatcherBundle\Services;
+use DispatcherBundle\Entity\JobRecord;
 use Doctrine\ORM\EntityManager;
 
 class WebLabDeustoServices
@@ -52,7 +53,7 @@ class WebLabDeustoServices
         if ($lab != null){
 
             $experimentSpecification = $params->client_initial_data;
-            $consumer_data = $params->consumer_data;
+            $consumer_data = $params->consumer_data;//save it to the opaque data field
 
             $this->labServerServices->setLabServerId($lab->getId());
             $submissionReport = $this->labServerServices->Submit(null, $experimentSpecification, $consumer_data, 0, $rlmsId);
@@ -73,10 +74,7 @@ class WebLabDeustoServices
     {
         $reservation_id = $params->reservation_id->id;
 
-        $jobRecord = $this
-            ->em
-            ->getRepository('DispatcherBundle:JobRecord')
-            ->findOneBy(array('expId' => $reservation_id, 'providerId' => $rlmsId));
+        $jobRecord = $this->labServerServices->getJobRecordById($reservation_id, $rlmsId);
 
         if ($jobRecord == null){
             return array('is_exception' => true,
@@ -84,18 +82,18 @@ class WebLabDeustoServices
                 'code' => 'Client.NoCurrentReservation');
         }
 
-        $this->labServerServices->setLabServerId($jobRecord->getLabserverId());
-        $expStatus = $this->labServerServices->getExperimentStatus($reservation_id, $rlmsId);
-
-        $statusCode = $expStatus['statusCode'];
+        $statusCode = $jobRecord->getJobStatus();
         switch($statusCode){
             case 1: //experiment has been queued
+
+                $this->labServerServices->setLabServerId($jobRecord->getLabserverId());
+                $expStatus = $this->labServerServices->getExperimentStatus($reservation_id, $rlmsId);
 
                 return array('is_exception' => false,
                     'result' => array('reservation_id' => array('id' => (string)$reservation_id),
                         'status' => 'Reservation::waiting',
                         'url' => 'http://localhost:8000/apis/weblab/',
-                        'position' => $expStatus['effectiveQueueLength']));//TODO: Change this, not nice!
+                        'position' => $expStatus['effectiveQueueLength']));
                 break;
             case 2: //Experiment is running
 
@@ -106,17 +104,127 @@ class WebLabDeustoServices
                 break;
             case 3: //Experiment has completed
 
-                $results = $this->labServerServices->retrieveResult($reservation_id, $rlmsId);
+                //$expResults = $this->labServerServices->retrieveResult($reservation_id, $rlmsId);
+                //$expSpecification = $this->labServerServices->retrieveExperimentSpecification($reservation_id, $rlmsId);
                 return array('is_exception' => false,
                     'result' => array('reservation_id' => array('id' => (string)$reservation_id),
-                        'status' => 'Reservation::post_reservation',
-                        'initial_data' => $results['experimentResults'],
-                        'end_data' => $results['experimentResults'],
-                        'finished' => true));
+                                      'status' => 'Reservation::post_reservation',
+                                      'initial_data' => $jobRecord->getExpSpecification(),
+                                      'end_data' => $jobRecord->getExpResults(),
+                                      'finished' => true));
+                break;
+            case 4: //Experiment has completed with errors
+
+                //$expResults = $this->labServerServices->retrieveResult($reservation_id, $rlmsId);
+                //$expSpecification = $this->labServerServices->retrieveExperimentSpecification($reservation_id, $rlmsId);
+                return array('is_exception' => false,
+                             'result' => array('reservation_id' => array('id' => (string)$reservation_id),
+                             'status' => 'Reservation::post_reservation',
+                             'initial_data' => $jobRecord->getExpSpecification(),
+                             'end_data' => $jobRecord->getExpResults(),
+                             'finished' => true));
+                break;
+            case 5: //Experiment has completed
+
+                //$expResults = $this->labServerServices->retrieveResult($reservation_id, $rlmsId);
+                //$expSpecification = $this->labServerServices->retrieveExperimentSpecification($reservation_id, $rlmsId);
+                return array('is_exception' => false,
+                             'result' => array('reservation_id' => array('id' => (string)$reservation_id),
+                             'status' => 'Reservation::post_reservation',
+                             'initial_data' => $jobRecord->getExpSpecification(),
+                              'end_data' => $jobRecord->getExpResults(),
+                              'finished' => true));
                 break;
             default:
                 return array('is_exception' => true,
                     'message' => "status code not available");
         }
+    }
+
+    public function getExperimentUseById($reservation_id, $rlmsId)
+    {
+        $jobRecord = $this->labServerServices->getJobRecordById($reservation_id, $rlmsId);
+
+        if ($jobRecord == null){
+            return $result = array('status' => 'forbidden');
+        }
+
+        $statusCode = $jobRecord->getJobStatus();
+        switch($statusCode){
+            case 1: //experiment has been queued
+
+                $this->labServerServices->setLabServerId($jobRecord->getLabserverId());
+                $result = array('status' => 'alive',
+                                'running' => false);
+                break;
+            case 2: //Experiment is running
+
+                $result = array('status' => 'alive',
+                                'running' => true);
+                break;
+            case 3: //Experiment has completed
+
+                $experimentUse = $this->assembleExperimentUse($jobRecord);
+
+                $result = array('status' => 'finished',
+                                'experiment_use' => $experimentUse);
+                break;
+            case 4:
+                $result = array('status' => 'cancelled');
+
+                break;
+            case 5:
+                $result = array('status' => 'cancelled');
+
+                break;
+            default:
+                $result = array('status' => 'forbidden');
+        }
+        return $result;
+    }
+
+    public function getExperimentUsesById($reservation_ids, $rlmsId)
+    {
+        $results = array();
+        foreach ($reservation_ids as $reservation_id){
+            $result = $this->getExperimentUseById($reservation_id->id, $rlmsId);
+            array_push($results, $result);
+        }
+        return $results;
+    }
+
+    private function assembleExperimentUse(JobRecord $jobRecord)
+    {
+        $expMetadata = $this->labServerServices->getExperimentMetadata($jobRecord);
+
+        $commands = array(array('command' => array('commandstring' => '@@@initial::request@@@'),
+                                'timestamp_before' => strtotime($jobRecord->getSubmitTime()),
+                                'timestamp_after' => strtotime($jobRecord->getExecutionTime()),
+                                'response' => array('commandstring' => $jobRecord->getExpSpecification())),
+                          array('command' => array('commandstring' => '@@@initial::response@@@'),
+                                'timestamp_before' => strtotime($jobRecord->getExecutionTime()),
+                                'timestamp_after' => strtotime($jobRecord->getEndTime()),
+                                'response' => array('commandstring' => $jobRecord->getExpResults())),
+                          array('command' => array('commandstring' => ' @@@finish@@@'),
+                                'timestamp_before' => strtotime($jobRecord->getSubmitTime()),
+                                'timestamp_after' => strtotime($jobRecord->getEndTime()),
+                                'response' => array('commandstring' => json_encode($expMetadata))));
+
+
+        $experimentUse = array('commands' => $commands,
+                               'end_date' => strtotime($jobRecord->getEndTime()),
+                               'experiment_use_id' => $jobRecord->getExpId(),
+                               'request_info' => json_decode($jobRecord->getOpaqueData()),
+                               'from_ip' => '127.0.0.1',
+                               'sent_files' => array(),
+                               'experiment_id' => array('exp_name' => $expMetadata['labServer']['exp_name'],
+                                                        'cat_name' => $expMetadata['labServer']['cat_name']),
+                               'coord_address' => array('process' => '',
+                                                        'host' => $expMetadata['labServer']['name'],
+                                                        'component' => $expMetadata['engine']['name']),
+                               'reservation_id' => $jobRecord->getExpId(),
+                               'start_date' => strtotime($jobRecord->getSubmitTime())
+        );
+        return $experimentUse;
     }
 }
